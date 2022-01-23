d72d83b7b765f2c7ef513cb77e2941178d88f681
Jason Deng <jgjdeng@gmail.com>
2022-01-22 23:25:23 -0600

    Adding users list state in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const usersListHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={usersListHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
